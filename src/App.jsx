583810d266946e78a57eee6478f9ff50f4433f60
hozaif <04072212010@student.qau.edu.pk>
2025-08-10 20:36:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+export default function App() {
+  const [todo, setTodo] = useState([]);
+  return <div></div>;
+}
