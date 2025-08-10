a1e77725a32de7d0bde6f808a69ed4bd3cb25222
hozaif <04072212010@student.qau.edu.pk>
2025-08-10 21:04:02 +0500

    implemented fetching data from api

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,18 @@ import { useState } from "react";
 import { useEffect } from "react";
 export default function App() {
   const [todo, setTodo] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTodo(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return <div></div>;
 }
